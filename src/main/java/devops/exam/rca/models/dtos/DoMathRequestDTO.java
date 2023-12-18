702694f3d3301f69fdc385337862aa669b5134a0
VanY <apple@APPLEs-MacBook-Pro.local>
2023-12-18 10:02:45 +0200

    [fix]: added integration and end2end tests

--- src/main/java/devops/exam/rca/models/dtos/DoMathRequestDTO.java
+++ src/main/java/devops/exam/rca/models/dtos/DoMathRequestDTO.java
@@ -1,13 +1,16 @@
 package devops.exam.rca.models.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class DoMathRequestDTO {
     @JsonProperty
     @NotNull
---
+++ src/test/java/devops/exam/rca/contollers/MathControllerIntegrationTest.java
@@ -0,0 +1,47 @@
+package devops.exam.rca.contollers;
+
+import devops.exam.rca.exceptions.InvalidOperationException;
+import devops.exam.rca.models.dtos.DoMathRequestDTO;
+import devops.exam.rca.services.IMathOperator;
+import devops.exam.rca.services.serviceImpl.MathOperatorImpl;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.testng.Assert;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class MathControllerIntegrationTest {
+
+    private IMathOperator mathOperatorService;
+
+    private static RestTemplate restTemplate;
+
+
+    @BeforeAll
+
+    public static void initializeVariables(){
+        restTemplate = new RestTemplate();
+    }
+
+    @Test
+    public void two_operands_when_added_return_sum() throws InvalidOperationException {
+        DoMathRequestDTO mathRequestDTO = new DoMathRequestDTO(4.0,4.0,"/");
+        ResponseEntity<?> response = restTemplate.exchange(
+                "http://localhost:8080/api/v1/math/do-math",
+                HttpMethod.POST,
+                new HttpEntity<>(mathRequestDTO, null),
+                DoMathRequestDTO.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+}
