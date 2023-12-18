7ce1f26cd24efa1434e64bd7273862e2f439bee0
VanY <apple@APPLEs-MacBook-Pro.local>
2023-12-18 08:52:41 +0200

    [feat]: initialized the project

---
+++ src/main/java/decops/exam/rca/RcaApplication.java
@@ -0,0 +1,13 @@
+package decops.exam.rca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/decops/exam/rca/RcaApplicationTests.java
@@ -0,0 +1,13 @@
+package decops.exam.rca;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcaApplicationTests {
+
+
+	@Test
+	void contextLoads() {
+	}
+
+}
