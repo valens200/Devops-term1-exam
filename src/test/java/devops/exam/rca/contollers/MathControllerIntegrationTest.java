f3344c0335215b097e4905992ea02ddcf787113a
VanY <apple@APPLEs-MacBook-Pro.local>
2023-12-18 10:31:13 +0200

    [feat]: added other test cases to unit test

--- src/test/java/devops/exam/rca/contollers/MathControllerIntegrationTest.java
+++ src/test/java/devops/exam/rca/contollers/MathControllerIntegrationTest.java
@@ -3,20 +3,16 @@ package devops.exam.rca.contollers;
 import devops.exam.rca.exceptions.InvalidOperationException;
 import devops.exam.rca.models.dtos.DoMathRequestDTO;
 import devops.exam.rca.services.IMathOperator;
-import devops.exam.rca.services.serviceImpl.MathOperatorImpl;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-import org.testng.Assert;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -24,9 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MathControllerIntegrationTest {
 
     private IMathOperator mathOperatorService;
-
     private static RestTemplate restTemplate;
-
 
     @BeforeAll
 
--- src/test/java/devops/exam/rca/services/MathServiceTest.java
+++ src/test/java/devops/exam/rca/services/MathServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 
 @ExtendWith(MockitoExtension.class)
 public class MathServiceTest {
@@ -14,15 +15,30 @@ public class MathServiceTest {
     @InjectMocks
     private MathOperatorImpl mathOperatorService;
 
+    private double operand1 = 4;
+    private double operand2 = 9;
+    private String operation = "+";
+
+    @DataProvider
+
     @Test
     public void two_operands_when_added_return_sum() throws InvalidOperationException {
-        double operand1 = 4;
-        double operand2 = 9;
-        String operation = "+";
-//        when(mathOperatorService.doMath(operand1, operand2, operation)).thenReturn(mathOperation);
         double actualMathOperator = mathOperatorService.doMath(operand1, operand2, operation);
-
         Assert.assertEquals(actualMathOperator,13.0);
+    }
+    @Test
+    public  void number_divided_with_zero_return_exception() throws InvalidOperationException {
+        org.junit.Assert.assertThrows("You can't devide by zero ",InvalidOperationException.class,() -> mathOperatorService.doMath(4.0, 0.0, "/"));
+    }
 
+    @Test void two_numbers_multiplied_return_product() throws InvalidOperationException {
+        Assert.assertEquals(mathOperatorService.doMath(4.0, 4.0, "*"),16.0);
+    }
+
+    @Test void two_numbers_added_return_sum() throws InvalidOperationException {
+        Assert.assertEquals(mathOperatorService.doMath(4.0, 4.0, "+"),8.0);
+    }
+    @Test void two_numbers_devided_return_quotient() throws InvalidOperationException {
+        Assert.assertEquals(mathOperatorService.doMath(4.0, 4.0, "/"),1.0);
     }
 }
